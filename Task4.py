4ecf03c1d3f0c15f9f92c514b402c48819314931
Jabez Jackson <jabezjackson10@gmail.com>
2025-08-28 18:35:12 +0530

    Create Task4.py

---
+++ Task4.py
@@ -0,0 +1,6 @@
+E = {0, 2, 4, 6, 8}
+N = {1, 2, 3, 4, 5}
+print("Union:", E.union(N))
+print("Intersection:", E.intersection(N))
+print("Difference:", E.difference(N))
+print("Symmetric Difference:", E.symmetric_difference(N))
